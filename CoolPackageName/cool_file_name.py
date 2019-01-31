4e9640013ebc5d70fc531826af73723f03d4d5fd
Gijs van den Abeele <gijsvdabeele@gmail.com>
2019-01-31 13:21:36 +0100

    I add twitter scraper that gets tweets from a given twitter url.

--- CoolPackageName/cool_file_name.py
+++ CoolPackageName/cool_file_name.py
@@ -1,6 +1,8 @@
-"""
-Module Docstring
-"""
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 __author__ = "Your Name"
 __version__ = "0.1.0"
@@ -9,11 +11,33 @@ __license__ = "MIT"
 
 def main():
     """ Main entry point of the app """
-    print(get_string())
+    get_twitter()
 
 
 def get_string():
     return 'Hello world!'
+
+
+def get_twitter():
+        chrome_options = Options()
+        #chrome_options.add_argument("--headless")
+        browser = webdriver.Chrome(chrome_options=chrome_options)
+        url = "https://twitter.com/search?q=%23Windmolens&src=typd&lang=nl"
+        browser.get(url)
+        time.sleep(1)
+
+        body = browser.find_element_by_tag_name('body')
+
+        for i in range(500):
+            body.send_keys(Keys.PAGE_DOWN)
+
+        tweets = browser.find_elements_by_class_name('tweet-text')
+
+        file = open("tweets.csv", mode="w", encoding="utf-16")
+        for tweet in tweets:
+            tweet = tweet.text.replace(',', '').replace('\n', '')
+            file.write(tweet+'\n')
+        file.close()
 
 
 if __name__ == "__main__":
